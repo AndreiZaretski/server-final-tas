ccb58078cb91ca26a9a4d55e4870f29acd524b30
AndreiZaretski <105275676+AndreiZaretski@users.noreply.github.com>
2023-02-20 10:32:01 +0300

    refactor: 8

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = require('./router');
 const cors = require('cors');
-const PORT = process.env.PORT ?? 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
